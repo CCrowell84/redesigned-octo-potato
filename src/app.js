89eb1ea073e73fc0299c48d00118ca29046bab03
Cooper Crowell <cooper.crowell@gmail.com>
2019-05-15 10:45:38 -0500

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs = require('hbs');
 const app = express();
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -91,6 +92,6 @@ app.get('*', (req, res) => {
     })
 });
 
-app.listen(3000, ()=> {
-    console.log('Server is up and running.')
+app.listen(port, ()=> {
+    console.log('Server is up and running on port ' + port);
 })
